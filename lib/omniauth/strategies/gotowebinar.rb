102713362a3a2fd3db831bd544b05b2bab55a53e
Graham Randall <grandall@normalmodes.com>
2013-04-29 16:02:55 -0700

    Finished G2W integration.

--- lib/omniauth/strategies/gotowebinar.rb
+++ lib/omniauth/strategies/gotowebinar.rb
@@ -11,59 +11,20 @@ module OmniAuth
         :token_url      => '/oauth/access_token'
       }
 
-      option :access_token_options, {
+      option :provider_ignores_state, true
+
+      option :token_params, {
         :header_format => 'OAuth oauth_token=%s',
         :param_name => 'access_token'
       }
 
-      option :token_params, {
-        :parse          => :json
-      }
-
-      uid do
-        raw_info['id']
+      credentials do
+        hash = {}
+        hash.merge!('organizer_key' => access_token['organizer_key'])
+        hash.merge!('account_key' => access_token['account_key'])
+        hash
       end
 
-      info do
-        {
-          :expires_in => raw_info['expires_in'],
-          :refresh_token => raw_info['refresh_token'],
-          :organizer_key => raw_info['organizer_key'],
-          :account_key => raw_info['account_key'],
-          :account_type => raw_info['account_type']
-        }
-      end
-
-      extra do
-        {
-          :raw_info => raw_info
-        }
-      end
-
-      def raw_info
-        @raw_info ||= access_token.get('/api/v1/account').parsed
-      end
-
-      ##
-      # You can pass +display+, +with_offical_account+ or +state+ params to the auth request, if
-      # you need to set them dynamically. You can also set these options
-      # in the OmniAuth config :authorize_params option.
-      #
-      # /auth/weibo?display=mobile&with_offical_account=1
-      #
-      def authorize_params
-        super.tap do |params|
-          %w[display with_offical_account state].each do |v|
-            if request.params[v]
-              params[v.to_sym] = request.params[v]
-
-              # to support omniauth-oauth2's auto csrf protection
-              session['omniauth.state'] = params[:state] if v == 'state'
-            end
-          end
-        end
-      end
-      
     end
   end
 end
